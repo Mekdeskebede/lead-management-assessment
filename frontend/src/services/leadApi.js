28d1fa0b7833a319fefcc72dfb0f193532887a75
mekdeskebede <kebedemekdes289@gmail.com>
2025-03-06 16:37:59 +0300

    fix:backend api

--- frontend/src/services/leadApi.js
+++ frontend/src/services/leadApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const leadApi = createApi({
   reducerPath: 'leadApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://lead-management-assessment.onrender.com/' }),
   tagTypes: ['Lead'],
   endpoints: (builder) => ({
     getLeads: builder.query({
